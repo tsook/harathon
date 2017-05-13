c724bdc448a938130c46938cef656ddfa6e17089
tsook <kimts0830@gmail.com>
2017-05-13 16:30:52 +0900

    login page changes

--- finance/views.py
+++ finance/views.py
@@ -6,11 +6,15 @@ from django.shortcuts import redirect
 
 def login_page(request):
 	form = loginForm()
+	Alllist = Relation.objects.all()
+	Getlist = Relation.objects.filter(receiver__lte="A")
+	Paylist = Relation.objects.filter(giver__lte="A")
 	if request.method == 'POST':
 		form = loginForm(request.POST)
 		if form.is_valid():
 			data = form.cleaned_data
-			return render(request, 'finance/home_page.html', {'name': data["name"]})
+
+			return render(request, 'finance/home_page.html', {'UserData' : Alllist, 'Getlist' : Getlist, 'Paylist' : Paylist, 'name': data["name"]})
 		else:
 			form = loginForm()
 
