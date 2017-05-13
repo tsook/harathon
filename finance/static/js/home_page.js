31b16645e3c92af052676d10c587e125dcc60046
tsook <kimts0830@gmail.com>
2017-05-13 14:34:08 +0900

    finalized basic homepage

--- finance/static/js/home_page.js
+++ finance/static/js/home_page.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-	$(".add-debt").on("click", function(){
-		$(".add-debt-form").show()
-	})
+	$(".add-debt").on("click", showForm);
 })
+
+function showForm(){
+	$(".add-debt-form").show()
+	$(".add-debt").removeClass("btn-success")
+	$(".add-debt").html("Hide")
+	$(".add-debt").off("click")
+	$(".add-debt").on("click", hideForm)
+}
+
+function hideForm(){
+	$(".add-debt-form").hide()
+	$(".add-debt").addClass("btn-success")
+	$(".add-debt").html("Add New Debt")
+	$(".add-debt").off("click")
+	$(".add-debt").on("click", showForm)
+}
